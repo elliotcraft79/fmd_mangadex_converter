12abd60842f99cde0e057d8a84354739c12804fb
elliotcraft79 <16109561+elliotcraft79@users.noreply.github.com>
2021-06-25 23:58:39 +0930

    clean up code, use prepared statements

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 For use with FMD2 in order to convert mangadex legacy IDs to UUIDs.
 
 ## How to use
-1. Copy fmd_mangadex_converter.py, and conversion.db to FMD folder.
-2. Run each of the scripts
+1. Copy fmd_mangadex_converter.py, and conversion.db into your FMD folder.
+2. Run fmd_mangadex_converter.py
 
-ps: there is backup copy for downloads.db, favorites.db, and downloadedchapters.db in the userdata folder.
+Note: there is backup copy for downloads.db, favorites.db, and downloadedchapters.db in the userdata folder.
--- fmd_mangadex_converter.py
+++ fmd_mangadex_converter.py
@@ -4,97 +4,146 @@ import sys
 import os
 import shutil
 
-DB_list = ['favorites.db', 'downloadedchapters.db', 'downloads.db', 'd07c9c2425764da8ba056505f57cf40c.db']
+DB_LIST = [
+    "favorites.db",
+    "downloadedchapters.db",
+    "downloads.db",
+    "d07c9c2425764da8ba056505f57cf40c.db",
+]
 
-def build_DB_path():
-    possiblity_path = ['.', 'userdata', 'data', '..', os.path.join('..', 'userdata'), os.path.join('..', 'data')]
-    DB_path = []
-    for DB_ in DB_list:
-        for path_ in possiblity_path:
-            if os.path.lexists(os.path.join(os.path.abspath(path_), DB_)):
-                DB_path += [os.path.join(os.path.abspath(path_), DB_)]
-    return DB_path
 
-def DB_convert(DB_, conversion_cur):
-    if os.path.basename(DB_) == 'd07c9c2425764da8ba056505f57cf40c.db':
-        skip_convert = input('do you want to skip converting mangadex manga database (it will take longtime)?>>')
-        if str(skip_convert).lower() == 'n':
-            pass
-        elif str(skip_convert).lower() == 'no':
+def build_db_path():
+    possible_paths = [
+        ".",
+        "userdata",
+        "data",
+        "..",
+        os.path.join("..", "userdata"),
+        os.path.join("..", "data"),
+    ]
+    db_path = []
+    for db in DB_LIST:
+        for path in possible_paths:
+            if os.path.lexists(os.path.join(os.path.abspath(path), db)):
+                db_path.append(os.path.join(os.path.abspath(path), db))
+    return db_path
+
+
+def convert_db(db_path, conversion_cur):
+    print(db_path)
+    db_name = os.path.basename(db_path)
+    if db_name == "d07c9c2425764da8ba056505f57cf40c.db":
+        skip_convert = input(
+            "Do you want to skip converting the mangadex manga database (it will take a while to convert)?\n>> "
+        )
+        if str(skip_convert).lower().startswith('n'):
             pass
         else:
             return
-        
-    con = sqlite3.connect(DB_)
 
-    sql_table_name = {'favorites.db': ['favorites', 'id', 0, 8],
-                      'downloadedchapters.db': ['downloadedchapters', 'id', 0, 1],
-                      'downloads.db': ['downloads', 'moduleid', 8, 15],
-                      'd07c9c2425764da8ba056505f57cf40c.db': ['masterlist', '', 0, 0]
-                      }[os.path.basename(DB_)]
-    
-    #cur = con.execute(f"SELECT * FROM '{sql_table_name[0]}'")
-    #print([column_des[0] for column_des in cur.description])
-    
+    con = sqlite3.connect(db_path)
+    con.row_factory = sqlite3.Row
+
+    sql_column_names = {
+        "favorites.db": {
+            "table": "favorites",
+            "id_column": "id",
+            "link_column": "id",
+            "chapters_column": "downloadedchapterlist",
+        },
+        "downloadedchapters.db": {
+            "table": "downloadedchapters",
+            "id_column": "id",
+            "link_column": "id",
+            "chapters_column": "chapters",
+        },
+        "downloads.db": {
+            "table": "downloads",
+            "id_column": "moduleid",
+            "link_column": "link",
+            "chapters_column": "chapterslinks",
+        },
+        "d07c9c2425764da8ba056505f57cf40c.db": {
+            "table": "masterlist",
+            "id_column": "",
+            "link_column": "link",
+            "chapters_column": "",
+        },
+    }[db_name]
     cur = con.cursor()
 
-    
-
-    #print(f"SELECT * FROM '{sql_table_name[0]}'" + ("" if sql_table_name[1] == "" else f"where {sql_table_name[1]} LIKE 'd07c9c2425764da8ba056505f57cf40c%'"))
-    cur.execute(f"SELECT * FROM '{sql_table_name[0]}'" + ("" if sql_table_name[1] == "" else f"where {sql_table_name[1]} LIKE 'd07c9c2425764da8ba056505f57cf40c%'"))
+    id_sql = (
+        f"WHERE {sql_column_names['id_column']} LIKE 'd07c9c2425764da8ba056505f57cf40c%'"
+        if sql_column_names["id_column"] != ""
+        else ""
+    )
+    cur.execute(f"SELECT * FROM {sql_column_names['table']} {id_sql}")
 
     mangadex_titles = cur.fetchall()
     already_converted = []
 
-    #print(DB_)
-    #print(mangadex_titles)
-    
     for title in mangadex_titles:
-        #print(len(title))
-        if re.match(r"^(?:d07c9c2425764da8ba056505f57cf40c)?\/title\/([0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12})", title[sql_table_name[2]]):
-            print(f"{title[sql_table_name[2]]} is already a UUID")
+        if re.match(
+            r"^(?:d07c9c2425764da8ba056505f57cf40c)?\/title\/([0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12})",
+            title[sql_column_names["link_column"]],
+        ):
+            print(f"{title[sql_column_names['link_column']]} is already a UUID")
             continue
-        title_match = re.match(r"^(?:d07c9c2425764da8ba056505f57cf40c)?\/title\/(?P<id>\d+)", title[sql_table_name[2]])
+        title_match = re.match(
+            r"^(?:d07c9c2425764da8ba056505f57cf40c)?\/title\/(?P<id>\d+)",
+            title[sql_column_names["link_column"]],
+        )
         if title_match == None:
             continue
-        title_id = title_match.group('id')
+        title_id = title_match.group("id")
         if title_id in already_converted:
             continue
-        conversion_cur.execute(f"SELECT * FROM manga_map WHERE legacy_id = '{title_id}'")
+        title_update_info = {"old_id": title[0]}
+        conversion_cur.execute("SELECT * FROM manga_map WHERE legacy_id = ?", (title_id,))
         title_mapping = conversion_cur.fetchone()
         if title_mapping:
-            new_title_link = '/title/' + title_mapping[1]
-            new_title_moudle_link = 'd07c9c2425764da8ba056505f57cf40c' + new_title_link
+            title_update_info["new_link"] = "/title/" + title_mapping["new_id"]
+            title_update_info["new_id"] = (
+                "d07c9c2425764da8ba056505f57cf40c" + title_update_info["new_link"]
+            )
         else:
             continue
-        print(f"{title_id} - {new_title_moudle_link}")
-        new_chapters = []
-        for chapter in title[sql_table_name[3]].strip().split('\n'):
-            conversion_cur.execute(f"SELECT * FROM chapter_map WHERE legacy_id = '{chapter.replace('/chapter/', '')}'")
-            chapter_mapping = conversion_cur.fetchone()
-            if chapter_mapping:
-                new_chapters.append(f"/{chapter_mapping[1]}")
-            else:
-                continue
-        new_chapters = '\n'.join(new_chapters) + '\n'
-        update_sql_command = {'favorites.db': f"UPDATE favorites SET id = '{new_title_moudle_link}', link = '{new_title_link}', downloadedchapterlist = '{new_chapters}' WHERE id = '{title[0]}'",
-                              'downloadedchapters.db': f"UPDATE downloadedchapters SET id = '{new_title_moudle_link}', chapters = '{new_chapters}' WHERE id = '{title[0]}'",
-                              'downloads.db': f"UPDATE downloads SET link = '{new_title_link}', chapterslinks = '{new_chapters}' WHERE id = '{title[0]}'",
-                              'd07c9c2425764da8ba056505f57cf40c.db': f"UPDATE masterlist SET link = '{new_title_link}' WHERE link = '{title[0]}'"
-                              }[os.path.basename(DB_)]
-        cur.execute(update_sql_command)
+        print(f"{title_id} -> {title_mapping['new_id']}")
+        if sql_column_names["chapters_column"] != "":
+            new_chapters = []
+            for chapter in title[sql_column_names["chapters_column"]].strip().split("\n"):
+                conversion_cur.execute(
+                    "SELECT * FROM chapter_map WHERE legacy_id = ?",
+                    (chapter.replace("/chapter/", ""),),
+                )
+                chapter_mapping = conversion_cur.fetchone()
+                if chapter_mapping:
+                    new_chapters.append(f"/{chapter_mapping['new_id']}")
+                else:
+                    continue
+            title_update_info["new_chapters"] = "\n".join(new_chapters) + "\n"
+        update_sql_command = {
+            "favorites.db": "UPDATE favorites SET id = :new_id, link = :new_link, downloadedchapterlist = :new_chapters WHERE id = :old_id",
+            "downloadedchapters.db": "UPDATE downloadedchapters SET id = :new_id, chapters = :new_chapters WHERE id = :old_id",
+            "downloads.db": "UPDATE downloads SET link = :new_link, chapterslinks = :new_chapters WHERE id = :old_id",
+            "d07c9c2425764da8ba056505f57cf40c.db": "UPDATE masterlist SET link = :new_link WHERE link = :old_id",
+        }[db_name]
+        cur.execute(update_sql_command, title_update_info)
         con.commit()
         already_converted.append(title_id)
 
-def converter():
-    DB_path = build_DB_path()
-    conversion_con = sqlite3.connect('conversion.db')
-    conversion_cur = conversion_con.cursor()
-    for DB_ in DB_path:
-        #make copy befoer start
-        shutil.copy2(DB_, os.path.splitext(DB_)[0]+'.bak')
 
-        DB_convert(DB_, conversion_cur)
-        
-if __name__ == '__main__':
+def converter():
+    db_path = build_db_path()
+    conversion_con = sqlite3.connect("conversion.db")
+    conversion_cur = conversion_con.cursor()
+    conversion_cur.row_factory = sqlite3.Row
+    for db in db_path:
+        # make a backup before starting
+        shutil.copy2(db, os.path.splitext(db)[0] + ".bak")
+
+        convert_db(db, conversion_cur)
+
+
+if __name__ == "__main__":
     converter()
